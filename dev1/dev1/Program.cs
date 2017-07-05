e55dc46cc4ab14867b2bccdc10b4fa659046e143
raul.trebuian <raul.trebuian@RODSK41018>
2017-07-05 09:38:36 +0300

    first commit

---
+++ dev1/dev1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Morning");
+        }
+    }
+}
